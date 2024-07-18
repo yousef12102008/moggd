dbcdc10ad82c3a5548bb8192c2a6878ffeab39bf
yousef12102008 <moh552vbnm@gmail.com>
2024-07-18 08:15:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 from chk2 import *
 from bin import *
 
-admin_id = '1790070248'
+admin_id = '1072224102'
 token = "6848019028:AAGDVZ4MIlMKOL0pRjtjMOadz4qkf9cqarU"
 bot = telebot.TeleBot(token, parse_mode="HTML")
 
